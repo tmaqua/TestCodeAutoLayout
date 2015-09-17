1db6da92cca3573e5365876e666a3cfb8d10c526
fankami <fankami.game@gmail.com>
2015-09-17 10:57:06 +0900

    pod install

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+use_frameworks!
+
+target 'TestCodeAutoLayout' do
+  pod 'Alamofire',  '~> 2.0'
+  pod 'SwiftyJSON', '~> 2.2.0'
+end
+
+target 'TestCodeAutoLayoutTests' do
+
+end
+
---
+++ TestCodeAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCodeAutoLayout
+//
+//  Created by 村上智紀 on 9/17/15.
+//  Copyright (c) 2015 planningdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
